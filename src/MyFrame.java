fd39b7105371bca71ed62c7b0335e4fe777ac3f8
Alexander Chatzigeorgiou <achat@uom.gr>
2020-01-14 18:30:53 +0200

    Initial Version

---
+++ src/MyFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	public MyFrame() {
+		
+		this.setVisible(true);
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
